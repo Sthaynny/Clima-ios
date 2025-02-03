820738e02561b7d2237b0630093ac152fecd9157
sthaynnyntt <igor.costanascimento@nttdata.com>
2025-02-03 17:08:20 -0300

    feat: create project clima

---
+++ ClimaTests/ClimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaTests.swift
+//  ClimaTests
+//
+//  Created by Igor Costa Nascimento on 03/02/25.
+//
+
+import Testing
+@testable import Clima
+
+struct ClimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
